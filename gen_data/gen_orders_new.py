a7c7e11a99db8cbc3e08313b70eb914563905283
duc-dn <ducdo23022001@gmail.com>
2023-04-03 09:14:30 +0700

    update generate orders and create new connector

--- gen_data/gen_orders_new.py
+++ gen_data/gen_orders_new.py
@@ -18,58 +18,59 @@ fake.add_provider(payment_type_provider)
 
 
 class mysql_connector():
-  def __init__(self, host, user, password, database):
-    self.host = host
-    self.user = user
-    self.password = password
-    self.database = database
-    
-  def connect_to_myql(self):
-    mydb = mysql.connector.connect(
-      host=self.host,
-      user=self.user,
-      password=self.password,
-      database=self.database
-    )
-    return mydb
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+
+    def __enter__(self):
+        self.conn = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+        self.cursor = self.conn.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is not None:
+            self.conn.rollback()
+        else:
+            self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
+
 
 if __name__ == "__main__":
-  
-  mydb = mysql_connector("mysql", "root", "debezium", "myshop")
-  
-  conn = mydb.connect_to_myql()
-  mycursor = conn.cursor()
+    with mysql_connector("mysql", "root", "debezium", "myshop") as cursor:
+      while True:
+        logger.info(20 * '-' + "inserting to orders table" + '-' * 20)
+        sql = "INSERT INTO orders (user_id, payment, status) value (%s, %s, %s)"
+        val = (random.randint(0, 10000), fake.payment_type(), "confirmed")
+        cursor.execute(sql, val)
 
-  while True:
-    logger.info(20*'-' + "inserting to orders table" + '-'*20)
-    sql = "INSERT INTO orders (user_id, payment) value (%s, %s)"
-    val = (random.randint(0, 10000), fake.payment_type())
-    mycursor.execute(sql, val)  
-    conn.commit()
+        # random products of a order
+        order_number = random.randint(0, 5)
+        logger.info(20 * '-' + "inserting to order_detail table" + '-' * 20)
+        order_id = cursor.lastrowid
 
-    # random products of a order
-    order_number = random.randint(0, 5)
-    logger.info(20*'-' + "inserting to order_detail table" + '-'*20)
-    order_id = mycursor.lastrowid
-    
-    order_details = []
-    for i in range(0, order_number):
+        order_details = []
+        for i in range(0, order_number):
 
-      product_id = random.randint(1, 695)
-      quantity = random.randint(1, 10)
+          product_id = random.randint(1, 695)
+          quantity = random.randint(1, 10)
 
-      logger.info(20*'-' + "get price from products" + '-'*20)
-      mycursor.execute(f"select price from products where id = {product_id}")
-      price = mycursor.fetchone()[0]
-      
-      order_details.append((order_id, product_id, quantity, price * quantity, int(local_time.timestamp())))
+          logger.info(20 * '-' + "get price from products" + '-' * 20)
+          cursor.execute(f"select price from products where id = {product_id}")
+          price = cursor.fetchone()[0]
 
+          order_details.append((order_id, product_id, quantity, price * quantity, int(local_time.timestamp())))
 
-    try:
-      sql = "INSERT INTO order_detail (order_id, product_id, quantity, price, create_at) VALUES (%s, %s, %s, %s, %s)"
-      mycursor.executemany(sql, order_details) 
-      conn.commit()
-    except Exception as e:
-      logger.error(e)
-
-    sleep(5)
+        try:
+          sql = "INSERT INTO order_detail (order_id, product_id, quantity, total_price, create_at) VALUES (%s, %s, %s, %s, %s)"
+          cursor.executemany(sql, order_details)
+        except Exception as e:
+          logger.error(e)
+        sleep(5)
